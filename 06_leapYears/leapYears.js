14a1a738d46e072fb3a057a916c00d2b25a2ef60
Daehee Lee <daehee.lee308@gmail.com>
2025-02-12 13:43:02 +0900

    solve leapYears example problem

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,34 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if(year%4==0)
+    {
+        if(year%100==0)
+        {
+            if(year%400==0)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        else return true;
+    }
+    else return false;
 
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+//pseudocode:
+//input param: year;
+//output: true/false;
+
+//if year %4 == 0 
+    //if year % 100 !=0 
+    //    if  year%400 ==0
+           //   return true;
+
+    //else 
+
