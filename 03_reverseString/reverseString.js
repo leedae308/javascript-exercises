b6dd61a48c509c72ed47a5ddc9a3b7bae1b28469
Daehee Lee <daehee.lee308@gmail.com>
2025-02-12 10:08:41 +0900

    complete reverse a string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+
+    let outputString="";
+
+    for(let i = inputString.length-1 ; i >= 0 ;  i--)
+    {
+        outputString+=inputString[i];
+    }
+
+    return outputString;
+
 
 };
 
