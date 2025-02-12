a4d557c8796009e2a103fed29b68c19553db0e07
Daehee Lee <daehee.lee308@gmail.com>
2025-02-12 10:03:06 +0900

    solve repeatString problem with TDD

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    let answer="";
+
+    if(num<0) return 'ERROR';  
+
+    for(let i=0; i<parseInt(num); i++)
+    {
+        answer+=string
+    }
+
+    return answer;
 
 };
 
