611e30e819ab38e693c8c4c26ca0c4ec03e1f9d2
Daehee Lee <daehee.lee308@gmail.com>
2025-02-12 13:49:54 +0900

    solve tempConversion example problem

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  const cels = (temp - 32) * 5 / 9;
+  // if (cels == 0) return 0;
+  return parseFloat(cels.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  const faren = (temp * 9 / 5 + 32);
+  // if (faren==0) return 0;
+  return parseFloat(faren.toFixed(1));
 };
 
 // Do not edit below this line
