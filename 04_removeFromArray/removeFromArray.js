15065e3b0689ffb3cd9f55a0bd0ad4693f842f55
Daehee Lee <daehee.lee308@gmail.com>
2025-02-12 11:09:32 +0900

    solve removeFromArray problem

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,38 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+
+    const resultArray = [];
+
+    if(arguments.length==1)
+    {
+        return array;
+    }
+    for(let i = 0 ; i < array.length ; i++)
+    {
+        for(let j = 1 ; j < arguments.length; j++)
+        {
+            if(array[i]===arguments[j])
+            {
+                delete(array[i]);
+                continue;
+            }
+        }
+    }
+
+    array.forEach((element)=>{
+        if(element!=undefined) resultArray.push(element);
+    })
+
+    return resultArray;
+
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+//Pseudocode
+//inputParams: array, ...nums
+//outputParam: resultArray
+
+//delete all values of equal to nums -> leaves hole of und.
+//result array -> move everything that the value is not und.
