629c8ceedeaf10017ce34cbfa974704a8c1b5066
Daehee Lee <daehee.lee308@gmail.com>
2025-02-19 10:16:33 +0900

    solve fibonacci using recursion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<0)
+        return "OOPS";
+    if(num==0)
+        return 0;
+    if(num==1)
+        return 1;
+    return fibonacci(num-1)+fibonacci(num-2);
 };
+
+// if num ==2: fib(1) + fib(0) = 1
+// if num==3: fib(2) + fib(1) = fib(1) + fib (0) + fib(1) = 2;
 
 // Do not edit below this line
 module.exports = fibonacci;
