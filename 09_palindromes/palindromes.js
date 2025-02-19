808ec29e5b48578f97de4692852d4592bb38bc3c
Daehee Lee <daehee.lee308@gmail.com>
2025-02-19 10:08:32 +0900

    solve palindromes using filter and match

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+        const newString = [...string.toLowerCase()].filter(letter => letter.match(/[a-z0-9]/i));
 
-};
+    for (let i = 0; i < newString.length / 2; i++) {
+        if (newString[i] !== newString[newString.length - i - 1]) return false;
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
